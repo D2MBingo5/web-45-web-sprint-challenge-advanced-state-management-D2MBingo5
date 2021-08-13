62835d26ed5eae77ef3a74cbf98c0e4e645ac4cf
Leu Family <romanianfobjr@gmail.com>
2021-08-13 12:31:05 -0500

    looks bleak

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
+import { fetchSmurfs } from './actions'
+
 class App extends Component {
   componentDidMount() {
-    axios.get('http://localhost:3333/smurfs')
-    .then(res => console.log(res))
-    .catch(err => console.log('Axios Error', err));
+    // axios.get('http://localhost:3333/smurfs')
+    // .then(res => console.log(res))
+    // .catch(err => console.log('Axios Error', err));
+    fetchSmurfs()
   }
 
   render() {
